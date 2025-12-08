800ca3b75f39f5b9d14c1b5bb2da9e281d83e839
Diego <diego.biagiol1@studenti.unipg.it>
2025-12-08 16:24:55 +0100

    2nd subrourine

--- project3/moldyn.f90
+++ project3/moldyn.f90
@@ -11,3 +11,16 @@ program mol_dyn
 
 
 end program mol_dyn
+
+
+subroutine read_mol(input_file, n, c, m)
+implicit none
+integer, intent(in) :: input_file
+integer :: i
+integer, intent(in) :: n
+doubleprecision, intent(out) :: c(n,3)
+doubleprecision, intent(out) :: m(n)
+do i=1,n
+read(*,*) c(i,1), c(i,2), c(i,3), m(i)
+end do
+end subroutine read_mol
