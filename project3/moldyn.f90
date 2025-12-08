9582892218dd019025e9e5ab954bb715316b36d3
Diego <diego.biagiol1@studenti.unipg.it>
2025-12-08 17:00:31 +0100

    V function

--- project3/moldyn.f90
+++ project3/moldyn.f90
@@ -24,3 +24,127 @@ do i=1,n
 read(*,*) c(i,1), c(i,2), c(i,3), m(i)
 end do
 end subroutine read_mol
+
+double precision function v(eps, sig, n, dis)
+        implicit none
+        integer :: d, l, p, i, t
+        double precision, intent(in) :: eps, sig
+        integer, intent(in) :: n
+        double precision, intent(in) :: dis(n,n)
+
+do i=1,n
+do t=i+1,n
+d=4*eps
+l=(sig/dis(i,t))**12
+p=(sig/dis(i,t))**6
+v=d*(l-p)
+end do
+end do
+end function v
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
