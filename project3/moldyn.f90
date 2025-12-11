aed8cea529328d1e0f56666818fdf360a82128c0
lorenzo <gialli.lorenzo4@gmail.com>
2025-12-11 18:48:17 +0100

    Implemented MD simulation and printing output file

--- project3/moldyn.f90
+++ project3/moldyn.f90
@@ -1,9 +1,11 @@
 program mol_dyn
 implicit none
-integer :: Natoms,read_Natoms,i,j
+integer :: Natoms,read_Natoms,i,j,nstep,l
 integer, parameter :: n=3                         ! number of dimension (xyz)
-integer, parameter :: unit_id=11                  ! id number for the unit
-character(len=20) :: file_name
+integer, parameter :: unit_id=11                  ! id number for the unit input
+integer, parameter :: out_id=12                   ! id number for the unit output
+double precision, parameter :: dt=0.02                     ! time step (ps)
+character(len=20) :: file_name,out_file
 double precision :: e_tot,V,T,eps,sig,pot,kin
 ! declaring array 
 double precision, allocatable :: xyz(:,:)
@@ -59,37 +61,68 @@ do i=1,n
  enddo
 enddo
 
-write(*,*)"Write sigma for LJ potential"
-read(*,*)sig
-write(*,*)"Write epsilon for LJ potential"
-read(*,*)eps
+write(*,*)"Write the number of step for yout MD simulation"
+read(*,*)nstep
 
-write(*,*)"STARTING COORDINATES AND MASS OF EACH ATOM"
-write(*, '(A6, 3A15, A14)') &
+
+write(*,*) "------------------------------------------------------------------------"
+read(unit_id,*)eps
+write(*,*)"Reading sigma for LJ potential:",eps,"kJ/mol" 
+read(unit_id,*)sig
+write(*,*)"Reading epsilon for LJ potential:",sig,"Angstrom"
+sig=sig*10.D0  ! conversion to nm
+
+
+write(*,*) "------------------------------------------------------------------------"
+!write(*,*)"STARTING COORDINATES AND MASS OF EACH ATOM"
+write(*, '(A6, 3A15, A18)') &
     adjustr("Atom"), &
-    adjustr("x"), &
-    adjustr("y"), &
-    adjustr("z"), &
-    adjustr("mass")
+    adjustr("x (Å)"), &
+    adjustr("y (Å)"), &
+    adjustr("z (Å)"), &
+    adjustr("mass (g/mol)")
 
 write(*,*) "------------------------------------------------------------------------"
 do i=1,Natoms
- write(*,'(I6, 3F15.8, F14.8)')i,(xyz(i,j), j=1, 3),mass(i)
+ write(*,'(I6, 3F15.8, F14.8)')i,(xyz(i,j)/10.D0, j=1, 3),mass(i)
 enddo
 
+write(*,*) "------------------------------------------------------------------------"
 
 call compute_distances(Natoms, xyz, dist)
 
 kin=T(Natoms,vel,mass)
-write(*,*)"KINETIC ENERGY =",kin
+write(*,*)"INITIAL KINETIC ENERGY =",kin,"kJ/mol"
 pot=V(eps,sig,Natoms,dist)
-write(*,*)"POTENTIAL ENERGY =",pot
+write(*,*)"INITIAL POTENTIAL ENERGY =",pot,"kJ/mol"
 e_tot=kin+pot
-write(*,*)"TOTAL ENERGY =",e_tot
+write(*,*)"TOTAL ENERGY =",e_tot,"kJ/mol"
+
+write(*,*) "------------------------------------------------------------------------"
+
+out_file=trim(file_name)//'_traj.xyz'
+open(unit=out_id, file=out_file) 
+
+do j=1,nstep/10
+ do i=1,10                                                                   ! progressing the system dynamic
+  call verlet_update(Natoms, xyz, mass, dist, eps, sig, vel, dt)
+ enddo
+ write(out_id,*)Natoms
+ write(out_id,*)"T=",T(Natoms,vel,mass),"kJ/mol","    V=",V(eps,sig,Natoms,dist),"kJ/mol","    Nstep=",j
+ do l=1,Natoms 
+  write(out_id,*)"Ar",xyz(l,1)/10.D0,xyz(l,2)/10.D0,xyz(l,3)/10.D0
+ enddo 
+enddo
 
 
+kin=T(Natoms,vel,mass)
+write(*,*)"FINAL KINETIC ENERGY =",kin,"kJ/mol"
+pot=V(eps,sig,Natoms,dist)
+write(*,*)"FINAL POTENTIAL ENERGY =",pot,"kJ/mol"
+e_tot=kin+pot
+write(*,*)"TOTAL ENERGY =",e_tot,"kJ/mol"
 
-
+write(*,*) "------------------------------------------------------------------------"
 
 ! deallocate the array
 deallocate(xyz)
@@ -142,6 +175,9 @@ doubleprecision, intent(out) :: c(n,3)
 doubleprecision, intent(out) :: m(n)
 do i=1,n
  read(input_file,*) c(i,1), c(i,2), c(i,3), m(i)
+ c(i,1)=c(i,1)*10.D0                                    ! conversion A->nm
+ c(i,2)=c(i,2)*10.D0
+ c(i,3)=c(i,3)*10.D0
 end do
 end subroutine read_mol
 
@@ -160,9 +196,10 @@ do i=1,n
   l=(sig/dis(i,t))**12
   p=(sig/dis(i,t))**6
   v=v+d*(l-p)
-  write(*,*)"l=",l,"p=",p,"d=",d,"v=",v,"           i=",i,"t=",t
+!  write(*,*)"l=",l,"p=",p,"d=",d,"v=",v,"           i=",i,"t=",t     debug line for checking value of the component of potential energy
  end do
 end do
+V=v
 end function V
 
 
@@ -174,13 +211,14 @@ double precision function T(Natoms, vel, mass)
         double precision, intent(in) :: mass(Natoms)
         double precision :: xvel, yvel, zvel
         integer :: i
+t=0.D0
 do i=1, Natoms
  xvel=vel(i,1)**2                       ! calculating the square of each component of the velocity
  yvel=vel(i,2)**2                       !        
  zvel=vel(i,3)**2                       !
- T=T+mass(i)*(xvel+yvel+zvel)           ! updating the total kinetic energy
+ t=t+mass(i)*(xvel+yvel+zvel)           ! updating the total kinetic energy
 enddo
-T=0.5D0*T
+T=0.5D0*t
 end function T
 
 
@@ -213,9 +251,9 @@ do i=1, Natoms
    az=az+u*(coord(i,3)-coord(j,3))/rij
   endif
  enddo 
- acc(i,1)=-1.D0/mass(i)*ax
- acc(i,2)=-1.D0/mass(i)*ay
- acc(i,3)=-1.D0/mass(i)*az
+ acc(i,1)=-(1.D0/mass(i))*ax
+ acc(i,2)=-(1.D0/mass(i))*ay
+ acc(i,3)=-(1.D0/mass(i))*az
 enddo
 
 
@@ -250,7 +288,7 @@ subroutine verlet_update(Natoms, coord, mass, dist, eps, sig, vel, dt)
         implicit none
         integer, intent(in) :: Natoms
         integer :: i,j
-        double precision, intent(in) :: dist(Natoms,Natoms)
+        double precision :: dist(Natoms,Natoms)
         double precision, intent(in) :: mass(Natoms)
         double precision :: coord(Natoms,3)
         double precision :: acc(Natoms,3)
@@ -264,6 +302,7 @@ do i=1,Natoms
   tempacc(j)=acc(i,j)                                                          !save the acceleration for the step n
   coord(i,j)=coord(i,j)+vel(i,j)*dt+(tempacc(j)*dt**2) /2.D0
  enddo
+ call compute_distances(Natoms, coord, dist)
  call compute_acc(Natoms, coord, mass, dist, acc, eps, sig)                  !calculating the new accelaration after the change of the posistions
  do j=1,3
   vel(i,j)=vel(i,j)+(acc(i,j)+tempacc(j))*dt/2.D0
