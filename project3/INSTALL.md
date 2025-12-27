c3573ba80bf159e9e5b9761311c6f895568c9cbc
lorenzo <gialli.lorenzo4@gmail.com>
2025-12-27 15:18:05 +0100

    Added test program for some function, and reorganized the workspace

--- project3/INSTALL.md
+++ project3/INSTALL.md
@@ -2,15 +2,16 @@
 
 With this file we want to furnish the user with the minimal instruction for compiling and 
 running the prrogram. Once installed the software, the user will be furnished of
-the moldyn.f90 code. This is the code used for molecular dynamic calculation.
+the necessary code in the src directory. This is the code used for molecular dynamic calculation.
 This code it's useless without an input file which contain all the data used for the
 calculation. As a matter of fact, the code moldyn.f90, it's designed for
 reading an external file and get from this external file all the necessary data for the calculation.
-After the installation the user will also get a test file, which is a file with randomic
-data, where the user will be able to understand how to set his own input data file, with
+After the installation the user will also get a test file, where the user will be able 
+to understand how to set his own input data file, with
 the right positions of the different data requested by the code. 
 Once prepared the input data file, as shown in test file, the user needs to compile
-the moldyn.f90 program with the following command: $ gfortran moldyn.f90 -o executable
+the moldyn.f90 program and the physics.f90 module with the following command: 
+$  gfortran src/physics.f90 src/moldyn.f90 -o executable
 The user can replace the word "executable" in the previous command with any word he likes.
 At this point the program has been compiled, and the user will be provided with an executable
 file, named as he chose before. The last thing to do now, is to launch the calculation of the executable.
--- project3/src/moldyn.f90
+++ project3/src/moldyn.f90
@@ -96,7 +96,7 @@ write(*,*)"INITIAL KINETIC ENERGY =",kin,"kJ/mol"
 pot=V(eps,sig,Natoms,dist)
 write(*,*)"INITIAL POTENTIAL ENERGY =",pot,"kJ/mol"
 e_tot=kin+pot
-write(*,*)"TOTAL ENERGY =",e_tot,"kJ/mol"
+write(*,*)"INITIAL TOTAL ENERGY =",e_tot,"kJ/mol"
 
 write(*,*) "------------------------------------------------------------------------"
 
@@ -120,7 +120,7 @@ write(*,*)"FINAL KINETIC ENERGY =",kin,"kJ/mol"
 pot=V(eps,sig,Natoms,dist)
 write(*,*)"FINAL POTENTIAL ENERGY =",pot,"kJ/mol"
 e_tot=kin+pot
-write(*,*)"TOTAL ENERGY =",e_tot,"kJ/mol"
+write(*,*)"FINAL TOTAL ENERGY =",e_tot,"kJ/mol"
 
 write(*,*) "------------------------------------------------------------------------"
 
@@ -181,45 +181,10 @@ do i=1,n
 end do
 end subroutine read_mol
 
-double precision function V(eps, sig, n, dis)
-        implicit none
-        integer ::  i, t
-        double precision, intent(in) :: eps, sig
-        integer, intent(in) :: n
-        double precision, intent(in) :: dis(n,n)
-        double precision :: d,l,p
-        
-v=0.D0
-do i=1,n
- do t=i+1,n
-  d=4.D0*eps
-  l=(sig/dis(i,t))**12
-  p=(sig/dis(i,t))**6
-  v=v+d*(l-p)
-!  write(*,*)"l=",l,"p=",p,"d=",d,"v=",v,"           i=",i,"t=",t     debug line for checking value of the component of potential energy
- end do
-end do
-V=v
-end function V
 
 
-! calculating the kinetic energy
-double precision function T(Natoms, vel, mass)
-        implicit none
-        integer, intent(in) :: Natoms
-        double precision, intent(in) :: vel(Natoms,3)
-        double precision, intent(in) :: mass(Natoms)
-        double precision :: xvel, yvel, zvel
-        integer :: i
-t=0.D0
-do i=1, Natoms
- xvel=vel(i,1)**2                       ! calculating the square of each component of the velocity
- yvel=vel(i,2)**2                       !        
- zvel=vel(i,3)**2                       !
- t=t+mass(i)*(xvel+yvel+zvel)           ! updating the total kinetic energy
-enddo
-T=0.5D0*t
-end function T
+
+
 
 
 
@@ -262,25 +227,6 @@ enddo
 end subroutine compute_acc
 
 
-
-
-
-
-
-
-
-! function calculating the energy potential derivate
-double precision function dpot(eps, sig, r) result(u)
-        implicit none
-        double precision, intent(in) :: eps, sig
-        double precision, intent(in) :: r
-        double precision :: d,l,p
-
-d=24.D0*eps/r
-l=2.D0*(sig/r)**12
-p=(sig/r)**6
-u=d*(p-l)
-end function dpot
 
 
 
