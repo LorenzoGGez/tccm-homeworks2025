9dd5da5c087fa6d94a06f012d5ce005701a8fb7a
lorenzo <gialli.lorenzo4@gmail.com>
2025-12-30 12:04:54 +0100

    sparse matrix multiplication subroutine added

--- project2a/src/sparse.f90
+++ project2a/src/sparse.f90
@@ -1,11 +1,12 @@
 program main
 use sparse_mod
 implicit none
-integer :: ndim1, ndim2, i 
+integer :: ndim1, ndim2, i, j, n_mul, n, i_stat 
+double precision :: y
 integer, allocatable :: R1(:), C1(:)
 double precision, allocatable :: V1(:)
 integer, allocatable :: R2(:), C2(:)
-double precision, allocatable :: V2(:)
+double precision, allocatable :: V2(:), C(:,:)
 character(len=50) :: filename
 
 
@@ -25,11 +26,31 @@ close(11)
 
 
 
+! check the dimension of the matrices for the multiplication
+if(ndim1 /= ndim2) stop "Mismatch in matrices dimension"
+n=ndim1
+
+allocate(C(n,n),  stat= i_stat)
+if(i_stat /= 0) then
+  print *, "MEMORY ALLOCATION FAILED"
+  stop
+end if
+
+
+call sparse_multiplication(R1, C1, V1, R2, C2, V2, C, n_mul, n)
+
+
+write(*,*)"Matrix product"
+do i=1, n
+ write(*,'(*(F12.6,1X))')C(i,:)
+enddo
+
+write(*,*)"Number of multiplication=",n_mul
 
 
 
 
-deallocate(R1, C1, V1, R2, C2, V2)
+deallocate(R1, C1, V1, R2, C2, V2, C)
 end program main
 
 
