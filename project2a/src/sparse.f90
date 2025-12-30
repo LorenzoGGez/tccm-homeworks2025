fea87015d70b8af5c3ac47b6889c2c567cf3cdfd
lorenzo <gialli.lorenzo4@gmail.com>
2025-12-30 17:20:17 +0100

    added desparsification and non-sparse matrix multiplication subroutine

--- project2a/src/sparse.f90
+++ project2a/src/sparse.f90
@@ -6,7 +6,8 @@ double precision :: y
 integer, allocatable :: R1(:), C1(:)
 double precision, allocatable :: V1(:)
 integer, allocatable :: R2(:), C2(:)
-double precision, allocatable :: V2(:), C(:,:)
+double precision, allocatable :: V2(:), C(:,:), mat1(:,:), mat2(:,:)
+double precision  :: t_start, t_end
 character(len=50) :: filename
 
 
@@ -37,7 +38,11 @@ if(i_stat /= 0) then
 end if
 
 
-call sparse_multiplication(R1, C1, V1, R2, C2, V2, C, n_mul, n)
+call cpu_time(t_start)
+do i=1, 100000
+ call sparse_multiplication(R1, C1, V1, R2, C2, V2, C, n_mul, n)
+enddo
+call cpu_time(t_end)
 
 
 write(*,*)"Matrix product"
@@ -46,11 +51,53 @@ do i=1, n
 enddo
 
 write(*,*)"Number of multiplication=",n_mul
+write(*,*)"Time for the operation",t_end-t_start
+
+
+! FIRST MATRIX
+! allocating the non-sparse format matrix
+allocate(mat1(ndim1,ndim1),  stat= i_stat)
+if(i_stat /= 0) then
+  print *, "MEMORY ALLOCATION FAILED"
+  stop
+end if
+
+! turning spare matrix in non-sparse format
+call desparsification(R1, C1, V1, mat1, ndim1)
+
+write(*,*)"Transforming the matrix in the non-sparse format"
+do i=1, ndim1
+ write(*,'(*(F12.6,1X))')mat1(i,:)
+enddo
+
+
+! SECOND MATRIX
+! allocating the non-sparse format matrix
+allocate(mat2(ndim2,ndim2),  stat= i_stat)
+if(i_stat /= 0) then
+  print *, "MEMORY ALLOCATION FAILED"
+  stop
+end if
+
+! turning spare matrix in non-sparse format
+call desparsification(R2, C2, V2, mat2, ndim2)
+
+write(*,*)"Transforming the matrix in the non-sparse format"
+do i=1, ndim2
+ write(*,'(*(F12.6,1X))')mat2(i,:)
+enddo
+
+! matrix multiplication with non-sparse format
+call matrices_prod(mat1, mat2, C, n)
+
+write(*,*)"Matrix product"
+do i=1, n
+ write(*,'(*(F12.6,1X))')C(i,:)
+enddo
 
 
 
-
-deallocate(R1, C1, V1, R2, C2, V2, C)
+deallocate(R1, C1, V1, R2, C2, V2, C, mat1, mat2)
 end program main
 
 
