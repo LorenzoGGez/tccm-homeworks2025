90a187782b6e133186226262ee4ad1099e81e851
lorenzo <gialli.lorenzo4@gmail.com>
2025-12-30 12:09:48 +0100

    Revert "sparse matrix multiplication subroutine added"
    This reverts commit 9dd5da5c087fa6d94a06f012d5ce005701a8fb7a.

--- project2a/src/sparse_mod.f90
+++ project2a/src/sparse_mod.f90
@@ -51,43 +51,4 @@ write(*,*) V
 
 end subroutine read_matrix
 
-subroutine sparse_multiplication(R1, C1, V1, R2, C2, V2, C, n_mul, ndim)
-implicit none
-integer :: p1, p2, i, j
-double precision :: y 
-integer, allocatable, intent(in) :: R1(:), C1(:), R2(:), C2(:)
-double precision, allocatable, intent(in) :: V1(:), V2(:)
-integer, intent(in) :: ndim
-
-integer, intent(out) :: n_mul
-double precision, intent(out) :: C(:,:)
-
-! inizializing the product matrix and the number of multiplication
-C=0.D0
-n_mul=0
-
-! multiplication
-do j=1, ndim        !rows of B
- do i=j, ndim    !rows of A  (exploiting the simmetry  of the matrices)
-  y = 0.0d0    !accumulator for the new matrix element
-  do p1=R1(i)+1,R1(i+1)   !number of non zero element per row of A
-   do p2=R2(j)+1,R2(j+1)    !number of non zero element per row of B 
-    if (C1(p1) == C2(p2)) then
-     y=y+V1(p1)*V2(p2)
-     n_mul=n_mul+1 
-    endif
-   enddo
-  enddo
-  C(i,j) = y
-  C(j,i) = y
- enddo
-enddo
-
-
-
-end subroutine sparse_multiplication
-
-
-
-
 end module sparse_mod
